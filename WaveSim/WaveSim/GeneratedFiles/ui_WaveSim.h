bdd4b9b548f199ffaf3c7e7c1a4028e29652034a
eternal75 <bennywang75@hotmail.com>
2017-09-14 22:52:58 -0700

    Added example code of how to programmatically access Qt Creator auto-generated code.

--- WaveSim/WaveSim/GeneratedFiles/ui_WaveSim.h
+++ WaveSim/WaveSim/GeneratedFiles/ui_WaveSim.h
@@ -15,7 +15,9 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,31 +27,61 @@ QT_BEGIN_NAMESPACE
 class Ui_WaveSimClass
 {
 public:
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionExit;
     QWidget *centralWidget;
+    QPushButton *pushButton;
     QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QMenu *menuFile;
+    QMenu *menuAbout;
     QStatusBar *statusBar;
+    QToolBar *mainToolBar;
 
     void setupUi(QMainWindow *WaveSimClass)
     {
         if (WaveSimClass->objectName().isEmpty())
             WaveSimClass->setObjectName(QStringLiteral("WaveSimClass"));
         WaveSimClass->resize(483, 370);
+        actionNew = new QAction(WaveSimClass);
+        actionNew->setObjectName(QStringLiteral("actionNew"));
+        actionOpen = new QAction(WaveSimClass);
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
+        actionSave = new QAction(WaveSimClass);
+        actionSave->setObjectName(QStringLiteral("actionSave"));
+        actionExit = new QAction(WaveSimClass);
+        actionExit->setObjectName(QStringLiteral("actionExit"));
         centralWidget = new QWidget(WaveSimClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(400, 290, 75, 23));
         WaveSimClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(WaveSimClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 483, 21));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuAbout = new QMenu(menuBar);
+        menuAbout->setObjectName(QStringLiteral("menuAbout"));
         WaveSimClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(WaveSimClass);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        WaveSimClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(WaveSimClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         WaveSimClass->setStatusBar(statusBar);
+        mainToolBar = new QToolBar(WaveSimClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        WaveSimClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuAbout->menuAction());
+        menuFile->addAction(actionNew);
+        menuFile->addAction(actionOpen);
+        menuFile->addAction(actionSave);
+        menuFile->addAction(actionExit);
 
         retranslateUi(WaveSimClass);
+        QObject::connect(actionExit, SIGNAL(triggered()), WaveSimClass, SLOT(close()));
 
         QMetaObject::connectSlotsByName(WaveSimClass);
     } // setupUi
@@ -57,6 +89,13 @@ public:
     void retranslateUi(QMainWindow *WaveSimClass)
     {
         WaveSimClass->setWindowTitle(QApplication::translate("WaveSimClass", "WaveSim", Q_NULLPTR));
+        actionNew->setText(QApplication::translate("WaveSimClass", "New", Q_NULLPTR));
+        actionOpen->setText(QApplication::translate("WaveSimClass", "Open", Q_NULLPTR));
+        actionSave->setText(QApplication::translate("WaveSimClass", "Save", Q_NULLPTR));
+        actionExit->setText(QApplication::translate("WaveSimClass", "Exit", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("WaveSimClass", "Close", Q_NULLPTR));
+        menuFile->setTitle(QApplication::translate("WaveSimClass", "File", Q_NULLPTR));
+        menuAbout->setTitle(QApplication::translate("WaveSimClass", "About", Q_NULLPTR));
     } // retranslateUi
 
 };
--- WaveSim/WaveSim/WaveSim.cpp
+++ WaveSim/WaveSim/WaveSim.cpp
@@ -1,7 +1,11 @@
 #include "WaveSim.h"
+#include <QObject>
+#include <QPushButton>
 
 WaveSim::WaveSim(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	
+	QObject::connect(ui.pushButton, SIGNAL (pressed()), this, SLOT(close()));
 }
