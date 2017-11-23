13fc1b5d9d26bf3359a92ff191b81d43642a4fd3
eternal75 <bennywang75@hotmail.com>
2017-11-22 17:11:35 -0800

    Added settings loading

--- WaveSim/WaveSim/SettingsManager.cpp
+++ WaveSim/WaveSim/SettingsManager.cpp
@@ -37,11 +37,20 @@ void SettingsManager::saveSettingsToFile()
 	for (auto it = mSettingsMap.begin(); it != mSettingsMap.end(); ++it)
 	{
 		string key(it->first);
-		settings.setValue(QString(key.c_str()), it->second);
+		settings.setValue(key.c_str(), it->second);
 	}
 }
 
 void SettingsManager::loadSettingsFromFile()
 {
 	QSettings settings(mSettingsFilename, QSettings::NativeFormat);
+	mSettingsMap[KEY_FPS] = settings.value(KEY_FPS.c_str()).toInt();
+	mSettingsMap[KEY_SIZE_X] = settings.value(KEY_SIZE_X.c_str()).toInt();
+	mSettingsMap[KEY_SIZE_Y] = settings.value(KEY_SIZE_Y.c_str()).toInt();
+	mSettingsMap[KEY_PIXEL_SIZE] = settings.value(KEY_PIXEL_SIZE.c_str()).toInt();
+	mSettingsMap[KEY_NEUTRAL_COLOR] = settings.value(KEY_NEUTRAL_COLOR.c_str()).toInt();
+	mSettingsMap[KEY_COLOR_CONTRAST] = settings.value(KEY_COLOR_CONTRAST.c_str()).toInt();
+	mSettingsMap[KEY_SHAPE_OPACITY] = settings.value(KEY_SHAPE_OPACITY.c_str()).toInt();
+	mSettingsMap[KEY_WINDOW_WIDTH] = mSettingsMap[KEY_SIZE_X] * mSettingsMap[KEY_PIXEL_SIZE];
+	mSettingsMap[KEY_WINDOW_HEIGHT] = mSettingsMap[KEY_SIZE_Y] * mSettingsMap[KEY_PIXEL_SIZE]
 }
